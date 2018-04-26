41277cfc6eec52b7dc794bcfc20eeed39f3c7582
Arjun Ankathatti Chandrashekara <acarjungowda@gmail.com>
2018-04-26 00:34:24 -0500

    second commit with python classes and marshmallow package for serializing and deserializing

--- cashman/index.py
+++ cashman/index.py
@@ -1,8 +1,29 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+
+from cashman.model.expense import Expense, ExpenseSchema
+from cashman.model.income import Income, IncomeSchema
+from cashman.model.transaction_type import TransactionType
+
 app = Flask(__name__)
 
+transactions = [
+	Income('Salary', 5000),
+	Income('Dividends', 200),
+	Expense('pizza', 50),
+	Expense('Rock Concert', 100)
+]
 
-@app.route("/")
+
+@app.route('/incomes')
+def get_incomes():
+	schema = IncomeSchema(many=True)
+	incomes = schema.dump(
+		filter(lambda t: t.type == TransactionType.INCOME, transactions)
+	)
+	return jsonify(incomes.data)
+
+@app.route("/incomes", methods=['POST'])
 def hello_world():
-	return "Hello, World!"
+	incomes.append(request.get_json())
+	return '', 204
 
